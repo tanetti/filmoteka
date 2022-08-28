6112af8b6709c642279d9622c384e310059ee39b
Pabearyoga <pabearyoga@gmail.com>
2022-08-29 00:07:22 +0300

    fix bug Official Trailer

--- src/js/modal/on-trailer-click.js
+++ src/js/modal/on-trailer-click.js
@@ -3,31 +3,27 @@ import { openModal } from './';
 
 export const onTrailerClick = async target => {
   const trailersData = await moviesFetcher.fetchVideos(target.dataset.movie);
-  // console.log(trailersData);
 
-  function renderTrailer() {
-    document.querySelector('.frame-wrapper').innerHTML = '';
-    let key;
+  document.querySelector('.frame-wrapper').innerHTML = '';
+  let key;
 
-    if (trailersData.length === 0) {
-      key = '2U76x2fD_tE';
-    } else {
-      key = trailersData[0].key;
-    }
-
-    trailersData.map(trailerData => {
-      if (trailerData.name === 'Official Trailer') {
-        key = trailerData.key;
-      }
-    });
-    const markup = `<iframe width="560" height="315" src='https://www.youtube.com/embed/${key}'frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
-
-    document
-      .querySelector('.frame-wrapper')
-      .insertAdjacentHTML('beforeend', markup);
+  if (trailersData.length === 0) {
+    key = '2U76x2fD_tE';
+  } else {
+    key = trailersData[0].key;
   }
 
-  renderTrailer();
+  trailersData.map(trailerData => {
+    if (trailerData.name === 'Official Trailer') {
+      key = trailerData.key;
+    }
+  });
+  const markup = `<iframe width="560" height="315" src='https://www.youtube.com/embed/${key}'frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+
+  document
+    .querySelector('.frame-wrapper')
+    .insertAdjacentHTML('beforeend', markup);
+  // }
 
   openModal(target);
 };
