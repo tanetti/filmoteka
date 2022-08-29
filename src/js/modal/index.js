604f393e6eb8e4baca5a5840264c9cb23f710fce
Volodymyr Dudka <tanetti22@gmail.com>
2022-08-29 13:02:33 +0300

    fix pagination button arrow click & add playlist to trailer modal4

--- src/js/modal/index.js
+++ src/js/modal/index.js
@@ -1,4 +1,5 @@
 export * from './on-movie-click';
 export * from './create-movie-modal-markup';
 export * from './on-trailer-click';
+export * from './create-trailer-modal-markup';
 export * from './open-modal';
--- src/js/modal/on-trailer-click.js
+++ src/js/modal/on-trailer-click.js
@@ -1,34 +1,39 @@
 import { moviesFetcher } from '../api';
-import { pageState } from '../state';
 import { openModal } from './';
+import { createTrailerModalMarkup } from './';
 
 export const onTrailerClick = async target => {
   openModal(target);
 
   const trailersData = await moviesFetcher.fetchVideos(target.dataset.movie);
 
-  let key = null;
+  let mainVideo = null;
+  const playList = [];
 
   if (trailersData.length === 0) {
-    key = '2U76x2fD_tE';
+    mainVideo = '2U76x2fD_tE';
   } else {
-    key = trailersData[0].key;
-  }
+    for (const trailer of trailersData) {
+      if (!mainVideo) {
+        mainVideo = trailer.key;
+        continue;
+      }
 
-  for (const trailer of trailersData) {
-    const name = trailer.name.toLowerCase();
+      const name = trailer.name.toLowerCase();
 
-    if (name.includes('official') || name.includes('офіційний')) {
-      key = trailer.key;
-      break;
+      if (name.includes('official') || name.includes('офіційний')) {
+        if (mainVideo) playList.push(mainVideo);
+
+        mainVideo = trailer.key;
+        continue;
+      }
+
+      playList.push(trailer.key);
     }
   }
 
-  const markup = `<iframe class="trailer-modal__farame is-hidden" type="text/html" width="560" height="315" src="https://www.youtube.com/embed/${key}?autoplay=1&showinfo=0&origin=https://tanetti.github.io&hl=${
-    pageState.locale === 'en' ? 'en' : 'uk'
-  }&controls=2" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
-
-  document.querySelector('.trailer-modal__farame-container').innerHTML = markup;
+  document.querySelector('.trailer-modal__farame-container').innerHTML =
+    createTrailerModalMarkup(mainVideo, playList);
   document
     .querySelector('.trailer-modal__farame')
     .classList.remove('is-hidden');
