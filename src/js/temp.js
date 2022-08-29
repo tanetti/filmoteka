8f1d4c1b95be9deca0f7d3ca7a085bdbb134be00
Volodymyr Dudka <tanetti22@gmail.com>
2022-08-29 20:38:40 +0300

    redesign scroll to top button

--- src/js/temp.js
+++ src/js/temp.js
@@ -1,8 +1,1 @@
-document.querySelector('.stt').onmousemove = function (e) {
 
-	var x = e.pageX - e.target.offsetLeft;
-	var y = e.pageY - e.target.offsetTop;
-
-	e.target.style.setProperty('--x', x + 'px');
-	e.target.style.setProperty('--y', y + 'px');
-};
