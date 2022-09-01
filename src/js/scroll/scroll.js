fe577fefe766c45dd445bae7ad8d011129af7105
Volodymyr Dudka <tanetti22@gmail.com>
2022-09-01 21:50:05 +0300

    add sticky header

--- src/js/scroll/scroll.js
+++ src/js/scroll/scroll.js
@@ -4,6 +4,12 @@ import { rootRefs } from '../root-refs';
 export const onWindowScroll = () => {
   const sttClassList = rootRefs.scrollToTopButton.classList;
 
+  if (window.scrollY >= MIN_OFFSET_FOR_SCROLL_TO_TOP_APEAR / 2) {
+    rootRefs.headerContainer.classList.add('is-fixed');
+  } else {
+    rootRefs.headerContainer.classList.remove('is-fixed');
+  }
+
   if (
     window.scrollY > MIN_OFFSET_FOR_SCROLL_TO_TOP_APEAR &&
     !sttClassList.contains('is-shown')
