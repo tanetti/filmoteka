b8ba26e2296e7da2af72d7f70f31e9d6296caece
Volodymyr Dudka <tanetti22@gmail.com>
2022-09-03 13:04:29 +0300

    trailer markup refactor

--- src/js/modal/create-trailer-modal-markup.js
+++ src/js/modal/create-trailer-modal-markup.js
@@ -2,11 +2,11 @@ import { pageState } from '../state';
 
 export const createTrailerModalMarkup = (mainVideo, playList) =>
   `<iframe class="trailer-modal__farame is-hidden" type="text/html" width="560" height="315"
-        src="https://www.youtube.com/embed/${
-          !playList.length ? mainVideo : ''
-        }?autoplay=1&showinfo=0&origin=https://tanetti.github.io&hl=${
+        src="https://www.youtube.com/embed${
+          playList.length > 0
+            ? `?playlist=${mainVideo},${playList.join(',')}&`
+            : `/${mainVideo}?`
+        }origin=https://tanetti.github.io&autoplay=1&showinfo=0&hl=${
     pageState.locale === 'en' ? 'en' : 'uk'
-  }&rel=0&iv_load_policy=3&enablejsapi=1${
-    playList.length > 0 ? `&playlist=${mainVideo},${playList.join(',')}` : ''
-  }" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope" allowfullscreen data-trailer_frame>
+  }&rel=0&iv_load_policy=3&enablejsapi=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope" allowfullscreen data-trailer_frame>
   </iframe>`;
