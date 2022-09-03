cbdf5bb7ebead2db06bad21eec60e96849fdba9c
Volodymyr Dudka <tanetti22@gmail.com>
2022-09-03 12:28:02 +0300

    add video autoplay

--- src/js/modal/create-trailer-modal-markup.js
+++ src/js/modal/create-trailer-modal-markup.js
@@ -2,9 +2,9 @@ import { pageState } from '../state';
 
 export const createTrailerModalMarkup = (mainVideo, playList) =>
   `<iframe class="trailer-modal__farame is-hidden" type="text/html" width="560" height="315"
-        src="https://www.youtube.com/embed/${mainVideo}?showinfo=0&origin=https://github.io&hl=${
+        src="https://www.youtube.com/embed/${mainVideo}?autoplay=1&showinfo=0&origin=https://tanetti.github.io&hl=${
     pageState.locale === 'en' ? 'en' : 'uk'
   }&rel=0&iv_load_policy=3&enablejsapi=1${
-    playList.length >= 1 ? `&playlist=${mainVideo},${playList.join(',')}` : ''
+    playList.length >= 1 ? `&playlist=${playList.join(',')}` : ''
   }" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope" allowfullscreen data-trailer_frame>
   </iframe>`;
