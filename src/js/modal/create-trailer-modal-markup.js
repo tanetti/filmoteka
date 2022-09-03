e8e643fad46194f8486c79a7a8829b53e04aa9bc
Volodymyr Dudka <tanetti22@gmail.com>
2022-09-03 12:39:32 +0300

    add video playlist options

--- src/js/modal/create-trailer-modal-markup.js
+++ src/js/modal/create-trailer-modal-markup.js
@@ -2,9 +2,11 @@ import { pageState } from '../state';
 
 export const createTrailerModalMarkup = (mainVideo, playList) =>
   `<iframe class="trailer-modal__farame is-hidden" type="text/html" width="560" height="315"
-        src="https://www.youtube.com/embed/${mainVideo}?autoplay=1&showinfo=0&origin=https://tanetti.github.io&hl=${
+        src="https://www.youtube.com/embed/${
+          !playList.length ? mainVideo : ''
+        }?autoplay=1&showinfo=0&origin=https://tanetti.github.io&hl=${
     pageState.locale === 'en' ? 'en' : 'uk'
   }&rel=0&iv_load_policy=3&enablejsapi=1${
-    playList.length >= 1 ? `&playlist=${playList.join(',')}` : ''
+    playList.length > 0 ? `&playlist=${mainVideo},${playList.join(',')}` : ''
   }" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope" allowfullscreen data-trailer_frame>
   </iframe>`;
